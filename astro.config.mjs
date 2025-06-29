719a378c05f71bf5eb0d58ae67c49b5f3986d30c
juansueldo <juan.sueldo1989@gmail.com>
2025-06-29 17:01:35 -0300

    URL fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     locales: ['en', 'es'],
     defaultLocale: 'es',
     routing:{
-      prefixDefaultLocale: false,
+      prefixDefaultLocale: true,
       redirectToDefaultLocale: false,
     }
   },
