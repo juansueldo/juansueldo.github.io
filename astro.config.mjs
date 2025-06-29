ce893d21f2717770e1e025a25bf78b95b5eb1f94
unknown <juan.sueldo1989@gmail.com>
2025-06-28 22:50:50 -0300

    Fixed domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  base: '/juansueldo.github.io/',
   site: 'https://juansueldo.dev/',
   i18n: {
     locales: ['en', 'es'],
