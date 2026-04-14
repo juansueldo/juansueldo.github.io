7bbf712bcb378f910753368ba328321e23a685e8
Juan Sueldo <juan.sueldo1989@gmail.com>
2026-04-14 20:39:27 -0300

    updated ip api

--- src/assets/js/ips.js
+++ src/assets/js/ips.js
@@ -1,5 +1,6 @@
 export const trackVisit = async () => {
     try {
+      const params = new URLSearchParams(window.location.search);
       await fetch("https://ipinfo-tawny-psi.vercel.app/api/ips", {
         method: "POST",
         headers: {
@@ -9,10 +10,29 @@ export const trackVisit = async () => {
           path: window.location.pathname,
           lang: document.documentElement.lang,
           userAgent: navigator.userAgent,
-          referrer: document.referrer || null
+          referrer: document.referrer || null,
+          utm_source: params.get('utm_source'),
+          utm_medium: params.get('utm_medium'),
+          utm_campaign: params.get('utm_campaign'),
+          utm_term: params.get('utm_term'),
+          utm_content: params.get('utm_content')
         })
       });
     } catch (error) {
       //console.error("Tracking error:", error);
+    }
+  };
+
+ export const pingBackend = async ()=> {
+    const apiUrl = "https://backend-gastronomico.onrender.com";
+    try {
+  
+      const res = await fetch(apiUrl);
+  
+  
+      const data = await res.text(); // o json()
+  
+    } catch (err) {
+      //console.error("Error en fetch:", err);
     }
   };
