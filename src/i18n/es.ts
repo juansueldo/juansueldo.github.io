86d1f18457913501e9b7b8069afc3e2ccb8063fb
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2026-04-24 08:35:01 -0300

    Fix project navigation label in Spanish localization

--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -7,8 +7,8 @@ export default {
     },
     nav:{
         about: "Sobre mí", 
-        projects: "Proyectos",
         experience: "Experiencia",
+        projects: "Proyectos",
         contact: "Contacto",
         light: "Claro",
         dark: "Oscuro",
