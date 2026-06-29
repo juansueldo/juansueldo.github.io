1a686f0b4979c66844bb495c57058ef7e539796d
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2026-06-29 15:16:25 -0300

    Update middleware.ts

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,9 +1,13 @@
-export const onRequest = ({ request, redirect }, next) => {
+import type { MiddlewareHandler } from "astro";
+
+export const onRequest: MiddlewareHandler = async (
+  { request, redirect },
+  next
+) => {
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
     const langHeader = request.headers.get("accept-language") || "";
-
     const lang = langHeader.startsWith("es") ? "es" : "en";
 
     return redirect(`/${lang}`);
